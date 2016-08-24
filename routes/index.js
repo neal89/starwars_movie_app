316370e55de1d6d752eb7029c84e39f0be037794
swapnil p. jadhav <nealja10@gmail.com>
2016-08-24 16:05:54 +0530

    new changes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var moviesJSON = require('../movies.json');
 // Home Route
 exports.home = function(req, res) {
@@ -55,61 +54,3 @@ exports.notFound =  function(req, res){
 	// res.send("This is not the page that you are looking for");
 
 };
-=======
-var moviesJSON = require('../movies.json');
-// Home Route
-exports.home = function(req, res) {
-	//res.send("<h3>This is a server response on the home page</h3>");
-	var movies = moviesJSON.movies;
-
-
-	res.render('home', {
-		title : "Star Wars Movies Homepage",
-		movies : movies,
-	});
-};
-
-// movie_single
-exports.movie_single =  function(req, res){
-	var episode_number = req.params.episode_number;
-
-	var movies = moviesJSON.movies;
-
-	if(episode_number>=1 && episode_number<=6) {
-
-		var movie = movies[episode_number - 1];
-
-		var title = movie.title;
-
-		var main_characters = movie.main_characters;
-
-		res.render('movie_single', {
-			movies : movies,
-			title : title,
-			movie : movie,
-			main_characters : main_characters
-		});
-	} else {
-
-		res.render('notFound', {
-			movies : movies,
-			title : "This is not the page you are looking for",
-		});
-	}
-	
-};
-
-// not found
-exports.notFound =  function(req, res){
-
-	var movies = moviesJSON.movies;
-
-	res.render('notFound', {
-			movies : movies,
-			title : "This is not the page you are looking for",
-		});
-
-	// res.send("This is not the page that you are looking for");
-
-};
->>>>>>> 7f154f84411db46db015b05743e64177f195afce
